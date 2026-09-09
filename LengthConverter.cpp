86583545b0f8775dc6168e4766b04e5a7694ea4e
Antony Jacob <antony.jacob@wavetronix.com>
2026-09-09 17:38:19 +0530

    Feat:meter to cenimeter

--- LengthConverter.cpp
+++ LengthConverter.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
-	int i,inp;
+	int i=1,inp;
 	while (true){
 		cout<<"================================\n";
-		cout<<"1. Meters to Kilometers\n2. Meters to Centimeters\n3. Meters to Millimeters\n4. Meters to Feet\n5. Meters to Inches\n6. Exit\n";
+		cout<<"1. Meters to Kilometers\n2. Meters to Centimeters\n3. Meters to Millimeters\n4. Meters to Feet\n5. Meters to Inches\n6. Exit";
 		cout<<"================================\n";
 		cout<<"Enter choice: ";
 		cin>>i;
