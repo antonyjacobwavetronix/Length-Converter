a47867a695d58462e7dca76effcf0548a64c64ae
Antony Jacob <antony.jacob@wavetronix.com>
2026-09-09 17:21:48 +0530

    Format changes made

--- LengthConverter.cpp
+++ LengthConverter.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
-	int i=1,inp;
+	int i,inp;
 	while (true){
 		cout<<"================================\n";
-		cout<<"1. Meters to Kilometers\n2. Meters to Centimeters\n3. Meters to Millimeters\n4. Meters to Feet\n5. Meters to Inches\n6. Exit";
+		cout<<"1. Meters to Kilometers\n2. Meters to Centimeters\n3. Meters to Millimeters\n4. Meters to Feet\n5. Meters to Inches\n6. Exit\n";
 		cout<<"================================\n";
 		cout<<"Enter choice: ";
 		cin>>i;
